4c961778707c9bb6e21d56504493ce9308e736bc
shirograhm <xavierwg13@gmail.com>
2026-08-06 00:48:16 -0700

    Update bloodletters_curse.rs

--- src/items/bloodletters_curse.rs
+++ src/items/bloodletters_curse.rs
@@ -122,6 +122,8 @@ impl StableItem for BloodlettersCurse {
         target: usize,
         _damage: &mut usize,
         damage_type: DamageTypeV1,
+        _attack_type: AttackTypeV1,
+        _is_crit: bool,
     ) {
         let Some(entity_ref) = ctx.get_entity(target) else {
             return;
